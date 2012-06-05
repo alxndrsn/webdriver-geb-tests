ad3d232cf5fa8565641416aaffc4efefd3e2ae18
Alex Anderson <alex@frontlinesms.com>
2012-06-05 08:48:27 +0300

    Extended test demonstrating failures of ghostscript's command interpretation.

--- src/test/groovy/example/AttributeSpec.groovy
+++ src/test/groovy/example/AttributeSpec.groovy
@@ -22,12 +22,16 @@ class AttributeSpec extends GebReportingSpec {
 			'invented' | 'ABC 123 four'
 	}
 
-	@Unroll
+	@Ignore // running this test has various nasty side effects - requesting /attribute/window
+	        // locks up the test suite, and another attribute (not sure which yet) kills ghostdriver
+	@Unroll('Fetching an attribute value called "#name" should be fully legal')
 	def 'attribute names that look like commands should be treated as attributes'() {
 		expect:
+			println "CHECKING ATTRIBUTE VALUE: $name"
 			keywords."@$name" == name.reverse()
 		where:
-			name << ['async_script', 'attribute', 'back', 'cookie', 'element', 'elements', 'enabled', 'equals', 'execute', 'execute_async', 'forward', 'frame', 'implicit_wait', 'name', 'refresh', 'screenshot', 'session', 'source', 'status', 'submit', 'tag', 'text', 'timeouts', 'url', 'value', 'window', 'window_handle', 'window_handles']
+			// This is a list of all words used in the URLs of commands the JSONWireProtocol (@see https://code.google.com/p/selenium/wiki/JsonWireProtocol)
+			name << ['accept_alert', 'activate', 'activated', 'active', 'active_engine', 'alert_text', 'async_script', 'attribute', 'available_engines', 'back', 'buttondown', 'buttonup', 'clear', 'click', 'cookie', 'css', 'deactivate', 'dismiss_alert', 'displayed', 'doubleclick', 'down', 'element', 'elements', 'enabled', 'equals', 'execute', 'execute_async', 'flick', 'forward', 'frame', 'ime', 'implicit_wait', 'key', 'keys', 'local_storage', 'location', 'location_in_view', 'longclick', 'maximize', 'move', 'moveto', 'name', 'orientation', 'position', 'refresh', 'screenshot', 'scroll', 'selected', 'session', 'session_storage', 'sessions', 'size', 'source', 'status', 'submit', 'text', 'timeouts', 'title', 'touch', 'up', 'url', 'value', 'window', 'window_handle', 'window_handles']
 	}
 }
 
